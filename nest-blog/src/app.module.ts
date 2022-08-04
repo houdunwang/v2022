f11ffd8d7d710f6d9ebcef310507d1725bce4d38
houdunren <2300071698@qq.com>
2022-08-04 19:21:39 +0800

    nest init

--- nest-blog/src/app.module.ts
+++ nest-blog/src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [],
@@ -8,3 +8,22 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+// function a(){}
+// a.prototype._module={
+//   {
+//     imports: [],
+//     controllers: [AppController],
+//     providers: [AppService],
+//   }
+// }
+
+// const modules = []
+// modules.push(a)
+// new a()
+// class B{
+//   protected a(){}
+// }
+// class A extends B{
+
+// }
