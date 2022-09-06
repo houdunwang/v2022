51bda7b12b240d76328fc34d80b7338e391bf234
houdunren <2300071698@qq.com>
2022-09-07 00:01:30 +0800

    重构后盾人网站启航

--- nest-houdunren/nest/src/config/data/aliyun.ts
+++ nest-houdunren/nest/src/config/data/aliyun.ts
@@ -2,8 +2,8 @@ import { registerAs } from '@nestjs/config'
 
 export const aliyuConfig = registerAs('aliyun', () => {
   return {
-    access_key: 'LTAI5t7Kktrp8sccQbYsQ92a',
-    access_secret: 'YP41PPTzD55frnwZFn17G8vmIOZdHm',
+    access_key: process.env.ALIYUN_ACCESS_KEY || '',
+    access_secret: process.env.ALIYUN_ACCESS_SECRET || '',
     sms_sign: '后盾人',
     sms_code_template: 'SMS_12840367',
   }
